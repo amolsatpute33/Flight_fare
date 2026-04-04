9c2a1b68851a0930fd0cc1d84fd43225019b4e5a
Amol Satpute <amolsatpute633@gmail.com>
2026-04-04 10:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import gdown
+import os
+
+# Google Drive file ID
+file_id = "1APQFw6h_IiDvLTIe_pK13iBzR6ioU03A"
+url = f"https://drive.google.com/uc?id={file_id}"
+
+# Download model if not exists
+if not os.path.exists("model.pkl"):
+    gdown.download(url, "model.pkl", quiet=False)
 
 # Load model
 model = pickle.load(open("model.pkl", "rb"))
-columns = pickle.load(open("columns.pkl", "rb"))
 
-st.title("✈️ Flight Fare Prediction App")
+# UI
+st.title("✈️ Flight Fare Prediction")
 
-st.write("Enter Flight Details:")
+st.write("Enter flight details:")
 
-# User Inputs
 airline = st.number_input("Airline", 0, 10)
 flight = st.number_input("Flight Code", 0, 2000)
 source_city = st.number_input("Source City", 0, 10)
@@ -19,18 +28,21 @@ stops = st.number_input("Stops", 0, 5)
 arrival_time = st.number_input("Arrival Time", 0, 10)
 destination_city = st.number_input("Destination City", 0, 10)
 class_type = st.number_input("Class", 0, 2)
-duration = st.number_input("Duration (hrs)", 0.0, 20.0)
+duration = st.number_input("Duration", 0.0, 20.0)
 days_left = st.number_input("Days Left", 0, 50)
 
-# Prediction button
 if st.button("Predict Price 💰"):
 
     input_data = pd.DataFrame([[airline, flight, source_city,
                                 departure_time, stops, arrival_time,
                                 destination_city, class_type,
                                 duration, days_left]],
-                              columns=columns)
+                              columns=[
+                                  'airline','flight','source_city','departure_time',
+                                  'stops','arrival_time','destination_city',
+                                  'class','duration','days_left'
+                              ])
 
     prediction = model.predict(input_data)
 
-    st.success(f"Estimated Flight Price: ₹ {int(prediction[0])}")
+    st.success(f"Estimated Price: ₹ {int(prediction[0])}")
